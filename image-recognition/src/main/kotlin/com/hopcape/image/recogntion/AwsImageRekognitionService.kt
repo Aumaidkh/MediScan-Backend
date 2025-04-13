202acb2948e485acdeb6871698ddf5c062a431d9
AumaidKh <murtaza@fourthfrontier.com>
2025-04-13 22:19:59 +0530

    Add documentation

--- image-recognition/src/main/kotlin/com/hopcape/image/recogntion/AwsImageRekognitionService.kt
+++ image-recognition/src/main/kotlin/com/hopcape/image/recogntion/AwsImageRekognitionService.kt
@@ -6,10 +6,28 @@ import software.amazon.awssdk.services.rekognition.RekognitionClient
 import software.amazon.awssdk.services.rekognition.model.DetectTextRequest
 import software.amazon.awssdk.services.rekognition.model.Image
 
+/**
+ * Service implementation for image recognition using AWS Rekognition.
+ *
+ * This service interacts with the AWS Rekognition service to detect and recognize text
+ * within an image. It leverages the RekognitionClient from the AWS SDK to send images
+ * and receive detected text results.
+ *
+ * @property client An instance of RekognitionClient to communicate with AWS Rekognition.
+ */
 @Service
-class AwsImageRekognitionService(
+internal class AwsImageRekognitionService(
     private val client: RekognitionClient
 ): ImageRecognitionService {
+    /**
+     * Recognizes and extracts text from the given image using AWS Rekognition.
+     *
+     * This method processes the provided image data as a byte array,
+     * sends it to the AWS Rekognition service, and retrieves the detected text content.
+     *
+     * @param imageBytes The image data as a byte array to be analyzed for text recognition.
+     * @return A string containing the recognized text from the image, concatenated with commas.
+     */
     override fun recognizeImage(imageBytes: ByteArray): String {
         // Convert the image bytes to SdkBytes
         val imageSdkBytes = SdkBytes.fromByteArray(imageBytes)
--- image-recognition/src/main/kotlin/com/hopcape/image/recogntion/ImageRecognitionService.kt
+++ image-recognition/src/main/kotlin/com/hopcape/image/recogntion/ImageRecognitionService.kt
@@ -1,5 +1,17 @@
 package com.hopcape.image.recogntion
 
+/**
+ * Interface for image recognition services.
+ *
+ * Provides a contract for implementing an image recognition service
+ * that processes an image and returns recognized data as a string.
+ */
 interface ImageRecognitionService {
+    /**
+     * Recognizes and extracts text or other data from the given image.
+     *
+     * @param imageBytes The image data as a byte array to be processed for recognition.
+     * @return A string representing the recognized content from the image.
+     */
     fun recognizeImage(imageBytes: ByteArray): String
 }
