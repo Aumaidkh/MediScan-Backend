d3a05c4cbd06c94d8979b79a50a5af9be3b4c5d4
AumaidKh <murtaza@fourthfrontier.com>
2025-04-21 13:57:31 +0530

    Refactoring changes. - Refactor clustering_algorithm build.gradle.kts

--- clustering_algorithm/build.gradle.kts
+++ clustering_algorithm/build.gradle.kts
@@ -7,26 +7,18 @@ plugins {
 }
 
 group = "com.hopcape.clustering"
-version = "unspecified"
+version = ProjectConfig.VERSION_NAME
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.apache.commons:commons-math3:3.6.1")
+    spring()
+    mongodb()
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    implementation("me.xdrop:fuzzywuzzy:1.4.0")
-
-    implementation(project(":medicine_management"))
-    implementation(project(":logging"))
-    implementation(project(":cache"))
+    addLogger()
+    addCache()
 }
 
 tasks.test {
